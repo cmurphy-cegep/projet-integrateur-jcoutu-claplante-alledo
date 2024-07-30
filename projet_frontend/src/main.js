526703aa2ce33aaca19c517b1d306e38e6783150
LaplanteC <laplantec.msi@gmail.com>
2024-07-30 17:24:44 -0400

    F2-debut-frontend

---
+++ projet_frontend/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+
+import ItemRecetteDetaillee from './pages/ItemRecetteDetaillee.vue';
+
+const app = createApp(App);
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/recettes/:id', component: ItemRecetteDetaillee, props: true },
+    ]
+});
+
+app.use(router);
+
+app.mount("#app");
