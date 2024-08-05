db3b8f262fbb61034493cf4d742d575d3a607dee
LaplanteC <laplantec.msi@gmail.com>
2024-08-05 11:23:16 -0400

    Update requetes pour ajout users dans bdd

--- projet_bdd/06_creer_utilisateurs.sql
+++ projet_bdd/06_creer_utilisateurs.sql
@@ -2,7 +2,7 @@ INSERT INTO Utilisateur (utilisateur_id, nom_complet, mot_de_passe_hash, mot_de_
 ('admin', 'Ricardo Larrivée', 'icIDUHEWCC9YEN22vlZy899NInMVOIYzcZNJzZFTPSYj3tVJRcvEM6Q9nUaLSo/9TD2XVmOJUCRKoot6yKambg==', 'L9UMT8Jxj6+u7E2NU4bw1A==', true);
 
 INSERT INTO Utilisateur (utilisateur_id, nom_complet, mot_de_passe_hash, mot_de_passe_sale) VALUES
-('test', 'admin test', 'icIDUHEWCC9YEN22vlZy899NInMVOIYzcZNJzZFTPSYj3tVJRcvEM6Q9nUaLSo/9TD2XVmOJUCRKoot6yKambg==', 'L9UMT8Jxj6+u7E2NU4bw1A==');
+('cmurphy', 'Charles Murphy', 'icIDUHEWCC9YEN22vlZy899NInMVOIYzcZNJzZFTPSYj3tVJRcvEM6Q9nUaLSo/9TD2XVmOJUCRKoot6yKambg==', 'L9UMT8Jxj6+u7E2NU4bw1A==');
 
 
 -- password = 12345
