3a78032ef8d90f8077588ca44d390c93b7a16aba
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-06 09:54:58 -0400

    pipeline to postgres test2

--- projet_backend/clients.js
+++ projet_backend/clients.js
@@ -14,13 +14,13 @@ pgclient.connect();
 // const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
 // const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
 
-pgclient.query(table, (err, res) => {
-    if (err) throw err
-});
+// pgclient.query(table, (err, res) => {
+//     if (err) throw err
+// });
 
-pgclient.query(text, values, (err, res) => {
-    if (err) throw err
-});
+// pgclient.query(text, values, (err, res) => {
+//     if (err) throw err
+// });
 
 pgclient.query('SELECT * FROM recettes', (err, res) => {
     if (err) throw err
