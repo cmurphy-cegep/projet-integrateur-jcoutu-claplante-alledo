1da40f9ac8b30bd9cf47d160e3d13fedf8c62770
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-06 09:53:55 -0400

    pipeline to postgres test1

--- projet_backend/clients.js
+++ projet_backend/clients.js
@@ -1,18 +1,18 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || 5432,
     user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+    password: 'Miriam10.',
+    database: 'recettes'
 });
 
 pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+// const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+// const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+// const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
 
 pgclient.query(table, (err, res) => {
     if (err) throw err
@@ -22,7 +22,7 @@ pgclient.query(text, values, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query('SELECT * FROM student', (err, res) => {
+pgclient.query('SELECT * FROM recettes', (err, res) => {
     if (err) throw err
     console.log(err, res.rows) // Print the data in student table
     pgclient.end()
