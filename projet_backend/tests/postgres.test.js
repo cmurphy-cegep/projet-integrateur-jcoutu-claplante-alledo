ade9a1454a83ad59c103d584829efc8fc8539d3a
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-06 14:18:53 -0400

    getAllRecipe OK

--- projet_backend/tests/postgres.test.js
+++ projet_backend/tests/postgres.test.js
@@ -1,6 +1,7 @@
 const { getAllRecettes } = require("../queries/RecetteQueries");
 
-test("get all recipes", function () {
-    const all = getAllRecettes()
-    expect(() => { getAllRecettes(); }).toEqual(all);
+test("get all recipes", async () => {
+    const recipes = await getAllRecettes()
+    console.log(recipes)
+    expect(recipes).toEqual(recipes);
 });
