607fa2a3de192ecced332b9d581ae63b4c98248e
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-12 11:05:28 -0400

    Eslint PASS

--- projet_backend/tests/recetteRoutes.test.js
+++ projet_backend/tests/recetteRoutes.test.js
@@ -10,25 +10,20 @@ describe("tests routes recettes", function () {
                 { id: 28, description: "Assembler les couches de pâtes, viande et sauce.", ordre: 2 },
                 { id: 29, description: "Faire cuire au four.", ordre: 3 }
             ];
-            return requete(app)
-                .get("/etapes/lasagnes")
-                .then((res) => {
-                    expect(res.statusCode).toBe(200);
-                    expect(res.body).toEqual(mockEtape);
-                })
+            const res = await requete(app).get("/etapes/lasagnes");
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual(mockEtape);
         });
-        it('Should return Error 404', () => {
+        it('Should return Error 404', async () => {
             mockReponseError = {
                 status: 404,
                 message: "Liste d'étapes recette-id-nexiste-pas-pour-test introuvable"
 
             }
-            return requete(app)
-                .get("/etapes/recette-id-nexiste-pas-pour-test")
-                .then((res) => {
-                    expect(res.statusCode).toBe(404);
-                    expect(res.body).toEqual(mockReponseError);
-                })
+            const res = await requete(app).get("/etapes/recette-id-nexiste-pas-pour-test");
+            expect(res.statusCode).toBe(404);
+            expect(res.body).toEqual(mockReponseError);
+            
         })
     });
 });
