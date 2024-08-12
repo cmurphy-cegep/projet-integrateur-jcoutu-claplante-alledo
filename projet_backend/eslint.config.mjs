bc38c8d65040af88ff372c34216ce7ea59aaff7b
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-12 10:13:57 -0400

    eslint + coverage pipeline

---
+++ projet_backend/eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import js from "@eslint/js";
+
+export default [
+  {files: ["**/*.js"],
+    languageOptions:
+      {sourceType: "commonjs"}
+  },
+
+  {languageOptions:
+     { globals: globals.node }
+    },
+
+  pluginJs.configs.recommended,
+  {
+    rules: {
+        "complexity": ["error", 10],
+        "max-depth": ["error", 4],
+        "max-lines": ["error", 300],
+        "max-lines-per-function": ["error", 50],
+        "max-nested-callbacks": ["error", 3],
+        "max-params": ["error", 6],
+        "max-statements": ["error", 10],
+        "no-var": "error",
+        "eqeqeq": ["error", "always"],
+    }
+}
+];
