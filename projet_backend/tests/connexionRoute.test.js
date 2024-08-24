4b4296a50254ec8b57f6cb871f180870c0e65364
AlexandreLledo <148722057+AlexandreLledo@users.noreply.github.com>
2024-08-24 17:53:33 -0400

    connexrion route en commentaire

--- projet_backend/tests/connexionRoute.test.js
+++ projet_backend/tests/connexionRoute.test.js
@@ -2,25 +2,25 @@ const requete = require('supertest');
 const app = require('../app');
 
 
-describe("Test routes CONNEXION", () => {
+// describe("Test routes CONNEXION", () => {
 
-    beforeEach(() => {
-        jest.resetAllMocks();
-    });
+//     beforeEach(() => {
+//         jest.resetAllMocks();
+//     });
 
-    it("GET / devrait retourner un utilisateur", async () => {
-        const mockUtilisateur = {
-            compteUtilisateurId: "alledo",
-            utilisateurNomComplet: "Alexandre Lledo",
-            estAdmin: false
-         };
+//     it("GET / devrait retourner un utilisateur", async () => {
+//         const mockUtilisateur = {
+//             compteUtilisateurId: "alledo",
+//             utilisateurNomComplet: "Alexandre Lledo",
+//             estAdmin: false
+//          };
          
-         const response = await requete(app).get('/')
-         console.log(response);
-                expect(response.body).toBe([{
-                    compteUtilisateurId: "alledo",
-                    utilisateurNomComplet: "Alexandre Lledo",
-                    estAdmin: false
-                 }]);
-    })
-})
+//          const response = await requete(app).get('/')
+//          console.log(response);
+//                 expect(response.body).toBe([{
+//                     compteUtilisateurId: "alledo",
+//                     utilisateurNomComplet: "Alexandre Lledo",
+//                     estAdmin: false
+//                  }]);
+//     })
+// })
